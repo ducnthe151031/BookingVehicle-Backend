d4ead34cd1870e3575d87aedaaa7bc1a5a42fe52
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-06-01 21:14:09 +0700

    update UserService

--- src/main/java/com/example/bookingvehiclebackend/service/UserService.java
+++ src/main/java/com/example/bookingvehiclebackend/service/UserService.java
@@ -1,4 +1,8 @@
 package com.example.bookingvehiclebackend.service;
 
+import com.example.bookingvehiclebackend.dto.User;
+
 public interface UserService {
+    User findByEmail(String email);
+    void updatePassword(User user, String newPassword);
 }
