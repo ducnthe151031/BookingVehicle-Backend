eb725fa919bfb083a1446b26617c94b93152f06a
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-06-10 00:18:32 +0700

    update controller

--- src/main/java/com/example/bookingvehiclebackend/v1/controller/AuthenController.java
+++ src/main/java/com/example/bookingvehiclebackend/v1/controller/AuthenController.java
@@ -4,20 +4,20 @@ import com.example.bookingvehiclebackend.utils.BaseApiResponse;
 import com.example.bookingvehiclebackend.v1.dto.request.AuthenRequest;
 import com.example.bookingvehiclebackend.v1.dto.response.LoginResponse;
 import com.example.bookingvehiclebackend.v1.service.AuthenService;
-import com.example.bookingvehiclebackend.v1.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
-import java.io.IOException;
+
 @RestController
 @RequestMapping(value = "v1/auth", produces = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class AuthenController {
     private final AuthenService authenService;
-    private final UserService userService;
 
     @PostMapping("/login")
     public BaseApiResponse<LoginResponse> login(@RequestBody AuthenRequest request) {
@@ -29,34 +29,9 @@ public class AuthenController {
         return BaseApiResponse.succeed(authenService.register(request, httpServletRequest));
     }
 
-    // TODO forgot password
-    // TODO CHANGE-PASSWORD
-    @GetMapping("/profile")
-    public BaseApiResponse<?> profile() {
-        return BaseApiResponse.succeed(userService.profile());
-    }
 
-    @PostMapping("/change-password")
-    public BaseApiResponse<Void> changePassword(@RequestBody AuthenRequest request, final HttpServletRequest httpServletRequest) {
-        authenService.changePassword(request);
-        return BaseApiResponse.succeed();
-    }
-
-    @PostMapping("/forgot-password")
-    public BaseApiResponse<LoginResponse> forgotPassword(@RequestBody AuthenRequest request, final HttpServletRequest httpServletRequest) {
-        return BaseApiResponse.succeed(authenService.forgotPassword(request,httpServletRequest));
-    }
-
-    @GetMapping("/verify-email")
-    public BaseApiResponse<Void> verifyEmail(@RequestParam String token,
-                                             HttpServletResponse response) throws IOException {
-        String successful = authenService.verifyEmail(token);
-        if ("Successful".equals(successful)) {
-            response.sendRedirect("/verification-success.html");
-        } else {
-            response.sendRedirect("/verification-failed.html");
-        }
-
-        return BaseApiResponse.succeed();
-    }
+//    @PutMapping("/profile")
+//    public BaseApiResponse<LoginResponse> updateProfile(@Path) {
+//        return BaseApiResponse.succeed(authenService.upProfile());
+//    }
 }
