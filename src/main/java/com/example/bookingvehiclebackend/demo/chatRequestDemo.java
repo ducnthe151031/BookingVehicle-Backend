e8369d4fa3c239e6b48fa101ea5414cb68a389b0
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-06-30 21:21:28 +0700

    create chatbot page demo

---
+++ src/main/java/com/example/bookingvehiclebackend/demo/chatRequestDemo.java
@@ -0,0 +1,21 @@
+package com.example.bookingvehiclebackend.demo;
+
+public class chatRequestDemo {
+    private String message;
+
+    // Constructors
+    public chatRequestDemo() {}
+
+    public chatRequestDemo(String message) {
+        this.message = message;
+    }
+
+    // Getter & Setter
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
