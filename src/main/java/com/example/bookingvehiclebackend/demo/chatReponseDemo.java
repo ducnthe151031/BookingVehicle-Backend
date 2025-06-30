501311fcd898f4ee5ff6741242271e55dbe8c184
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-06-30 21:20:57 +0700

    create chatbot basis response

---
+++ src/main/java/com/example/bookingvehiclebackend/demo/chatReponseDemo.java
@@ -0,0 +1,21 @@
+package com.example.bookingvehiclebackend.demo;
+
+public class chatReponseDemo {
+    private String reply;
+
+    public chatReponseDemo(String reply) {
+        this.reply = reply;
+    }
+
+    public chatReponseDemo() {
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+}
+
