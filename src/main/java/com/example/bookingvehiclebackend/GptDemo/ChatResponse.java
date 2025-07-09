94b8ff7877e1b6029be8a0e8326e5d5aec197346
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-07-09 20:29:32 +0700

    update chat gpt request and response

---
+++ src/main/java/com/example/bookingvehiclebackend/GptDemo/ChatResponse.java
@@ -0,0 +1,12 @@
+package com.example.bookingvehiclebackend.GptDemo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ChatResponse {
+    private String response;
+}
+
