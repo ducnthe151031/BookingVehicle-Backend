61f814d8e73657f60900947edf4126e3a8d4ae74
nguyenduck222001 <maususu11@gmail.com>
2025-07-19 14:59:47 +0700

    update UserRepository

--- src/main/java/com/example/bookingvehiclebackend/v1/repository/UserRepository.java
+++ src/main/java/com/example/bookingvehiclebackend/v1/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.bookingvehiclebackend.v1.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.Optional;
 
 @Repository
