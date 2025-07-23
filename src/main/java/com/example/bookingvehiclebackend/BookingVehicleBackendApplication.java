779dceb4bff77da5a1b800acf4c00d1b1753617e
DUCNTHE151031 <ducnthe151031@fpt.edu.vn>
2025-07-23 15:41:47 +0700

    update BookingVehicleBackendApplication, clear db, comment @EnableScheduling

--- src/main/java/com/example/bookingvehiclebackend/BookingVehicleBackendApplication.java
+++ src/main/java/com/example/bookingvehiclebackend/BookingVehicleBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.bookingvehiclebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
 public class BookingVehicleBackendApplication {
 
     public static void main(String[] args) {
