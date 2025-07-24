545969cc055e8e5bafb36993b52486f02be8e4ff
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-07-24 21:09:07 +0700

    update admin service

--- src/main/java/com/example/bookingvehiclebackend/v1/service/AdminService.java
+++ src/main/java/com/example/bookingvehiclebackend/v1/service/AdminService.java
@@ -2,11 +2,8 @@ package com.example.bookingvehiclebackend.v1.service;
 
 import com.example.bookingvehiclebackend.v1.dto.Brand;
 import com.example.bookingvehiclebackend.v1.dto.Category;
-import com.example.bookingvehiclebackend.v1.dto.User;
 import com.example.bookingvehiclebackend.v1.dto.VehicleType;
-import com.example.bookingvehiclebackend.v1.dto.request.AuthenRequest;
 import com.example.bookingvehiclebackend.v1.dto.request.CreateVehicleRequest;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.io.IOException;
@@ -21,7 +18,7 @@ public interface AdminService {
 
     Object approveBooking(String id);
 
-    Object searchVehicles(List<String> brands, List<String> categories, String vehicleName, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable, String status,String fuelType);
+    Object searchVehicles(List<String> brands, List<String> categories, String vehicleName, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable, String status);
 
     Object viewVehicle(String id);
 
@@ -29,7 +26,7 @@ public interface AdminService {
 
     void deleteVehicle(CreateVehicleRequest request);
 
-    Object rentalList(List<String> brands, List<String> categories, String vehicleName, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable, String status,String fuelType);
+    Object rentalList(List<String> brands, List<String> categories, String vehicleName, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable, String status);
 
     List<VehicleType> vehicleTypeList();
 
@@ -37,35 +34,5 @@ public interface AdminService {
 
     void approveVehicle(CreateVehicleRequest request);
 
-    Object searchVehiclesIsApproved(List<String> brands, List<String> categories, String vehicleName, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable, String status,String fuelType);
-
-    Object createBrand(Brand brand);
-
-    Object updateBrand(String id, Brand brand);
-
-    void deleteBrand(String id);
-
-    Object createCategory(Category category);
-
-    Object updateCategory(String id, Category category);
-
-    void deleteCategory(String id);
-
-    List<User> getUserList();
-
-
-    void deleteUser(String id);
-
-    Object updateUserRole(String id, User user);
-
-    void rejectVehicle(CreateVehicleRequest request);
-
-    Object returnedBooking(String id);
-
-    Object deliveredBooking(String id);
-
-    Object getListUser();
-
-    Object createUserList(AuthenRequest request, HttpServletRequest httpServletRequest);
-
+    Object searchVehiclesIsApproved(List<String> brands, List<String> categories, String vehicleName, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable, String status);
 }
