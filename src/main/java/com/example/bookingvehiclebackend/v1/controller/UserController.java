014828333da64f33ba76f7130515fb476f30ffa4
HieuNM18012003 <hieunmhe170629@fpt.edu.vn>
2025-08-04 20:51:44 +0700

    update user service

--- src/main/java/com/example/bookingvehiclebackend/v1/controller/UserController.java
+++ src/main/java/com/example/bookingvehiclebackend/v1/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.example.bookingvehiclebackend.v1.controller;
 
 import com.example.bookingvehiclebackend.utils.BaseApiResponse;
+import com.example.bookingvehiclebackend.v1.dto.DeliveryStatus;
 import com.example.bookingvehiclebackend.v1.dto.RentalRequest;
 import com.example.bookingvehiclebackend.v1.dto.Vehicle;
 import com.example.bookingvehiclebackend.v1.dto.request.*;
+import com.example.bookingvehiclebackend.v1.exception.PvrsClientException;
+import com.example.bookingvehiclebackend.v1.exception.PvrsErrorHandler;
 import com.example.bookingvehiclebackend.v1.repository.RentalRequestRepository;
 import com.example.bookingvehiclebackend.v1.repository.VehicleRepository;
 import com.example.bookingvehiclebackend.v1.service.AuthenService;
 import com.example.bookingvehiclebackend.v1.service.UserService;
+import com.example.bookingvehiclebackend.v1.utils.SecurityUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
@@ -41,9 +46,15 @@ public class UserController {
 
     @PostMapping("/bookings")
     public BaseApiResponse<?> bookingVehicle(@RequestBody BookingVehicleRequest request) throws Exception {
-        return BaseApiResponse.succeed(userService.bookingVehicle
-                (request));
+        return BaseApiResponse.succeed(userService.bookingVehicle(request));
     }
+
+    @PostMapping("/payLateFee/{id}")
+    public BaseApiResponse<?> payLateFee(@PathVariable String id) throws Exception {
+        return BaseApiResponse.succeed(userService.payLateFee(id));
+    }
+
+
     @GetMapping("/profile")
     public BaseApiResponse<?> profile() {
         return BaseApiResponse.succeed(userService.profile());
@@ -66,6 +77,7 @@ public class UserController {
             return ResponseEntity.badRequest().build();
         }
     }
+
     @PostMapping("/change-password")
     public BaseApiResponse<Void> changePassword(@RequestBody AuthenRequest request) {
         userService.changePassword(request);
@@ -81,18 +93,19 @@ public class UserController {
     }
 
     @GetMapping("/verify-email")
-    public BaseApiResponse<Void> verifyEmail(@RequestParam String token,
+    public BaseApiResponse<Void> verifyEmail(@RequestParam("token") String token,
                                              HttpServletResponse response) throws IOException {
         String successful = userService.verifyEmail(token);
         if ("Successful".equals(successful)) {
             response.sendRedirect("/verification-success.html");
+        } else if ("User has been verified".equals(successful)) {
+            response.sendRedirect("/user-has-been-verified.html");
         } else {
             response.sendRedirect("/verification-failed.html");
         }
 
         return BaseApiResponse.succeed();
     }
-
 
 
     @PutMapping("/profile")
@@ -102,36 +115,64 @@ public class UserController {
 
 
     @GetMapping("/payment/success")
-    public void handlePaymentSuccess(@RequestParam("orderCode") long orderCode, HttpServletResponse response) throws IOException {
+    public void handlePaymentSuccess(@RequestParam("orderCode") long orderCode, HttpServletResponse response, HttpServletRequest request) throws IOException {
         // Tìm rental request dựa trên orderCode
         RentalRequest rr = rentalRequestRepository.findByOrderCode(orderCode)
-                .orElseThrow(() -> new RuntimeException("Rental request not found"));
+                .orElseThrow(PvrsClientException.supplier(PvrsErrorHandler.RENTAL_REQUEST_NOT_FOUND));
         rr.setPaymentStatus(true);
         rr.setStatus("PENDING");
+        rr.setDeliveryStatus(DeliveryStatus.READY_TO_PICK.name());
         // Cập nhật trạng thái xe thành "rented"
         Vehicle v = vehicleRepository.findById(rr.getVehicleId())
-                .orElseThrow(() -> new RuntimeException("Vehicle not found"));
+                .orElseThrow(PvrsClientException.supplier(PvrsErrorHandler.VEHICLE_NOT_FOUND));
         v.setStatus("PENDING");
+        rr.setDeliveryStatus(DeliveryStatus.READY_TO_PICK.name());
         vehicleRepository.save(v);
-
+        String frontendUrl = "http://localhost:5173";
         // Chuyển hướng về trang home
-        response.sendRedirect("http://localhost:5173/home"); // Thay bằng domain thực tế của frontend
+        response.sendRedirect(frontendUrl + "/home"); // Thay bằng domain thực tế của frontend
     }
 
     @GetMapping("/payment/failed")
-    public void handlePaymentFailed(@RequestParam("orderCode") long orderCode, HttpServletResponse response) throws IOException {
+    public void handlePaymentFailed(@RequestParam("orderCode") long orderCode, HttpServletResponse response,HttpServletRequest request) throws IOException {
         // Tìm rental request dựa trên orderCode
         RentalRequest rr = rentalRequestRepository.findByOrderCode(orderCode)
-                .orElseThrow(() -> new RuntimeException("Rental request not found"));
+                .orElseThrow(PvrsClientException.supplier(PvrsErrorHandler.RENTAL_REQUEST_NOT_FOUND));
         rr.setPaymentStatus(false);
         // Cập nhật trạng thái xe thành "AVAILABLE"
         Vehicle v = vehicleRepository.findById(rr.getVehicleId())
-                .orElseThrow(() -> new RuntimeException("Vehicle not found"));
+                .orElseThrow(PvrsClientException.supplier(PvrsErrorHandler.VEHICLE_NOT_FOUND));
         v.setStatus("AVAILABLE");
         vehicleRepository.save(v);
-
+        String frontendUrl = "http://localhost:5173";
         // Chuyển hướng về trang home
-        response.sendRedirect("http://localhost:5173/home"); // Thay bằng domain thực tế của frontend
+        response.sendRedirect(frontendUrl + "/home"); // Thay bằng domain thực tế của frontend
+    }
+
+
+    @GetMapping("/late/success")
+    public void handlePaymentLateSuccess(@RequestParam("orderCode") long orderCode, HttpServletResponse response, HttpServletRequest request) throws IOException {
+        // Tìm rental request dựa trên orderCode
+        RentalRequest rr = rentalRequestRepository.findByOrderCode(orderCode)
+                .orElseThrow(PvrsClientException.supplier(PvrsErrorHandler.RENTAL_REQUEST_NOT_FOUND));
+        rr.setLateFeePaid(true);
+        rentalRequestRepository.save(rr);
+        String frontendUrl = "http://localhost:5173";
+        // Chuyển hướng về trang home
+        response.sendRedirect(frontendUrl + "/home"); // Thay bằng domain thực tế của frontend
+    }
+
+    @GetMapping("/late/failed")
+    public void handlePaymentLateFailed(@RequestParam("orderCode") long orderCode, HttpServletResponse response,HttpServletRequest request) throws IOException {
+        // Tìm rental request dựa trên orderCode
+        RentalRequest rr = rentalRequestRepository.findByOrderCode(orderCode)
+                .orElseThrow(PvrsClientException.supplier(PvrsErrorHandler.RENTAL_REQUEST_NOT_FOUND));
+        rr.setLateFeePaid(false);
+        rentalRequestRepository.save(rr);
+
+        String frontendUrl = "http://localhost:5173";
+        // Chuyển hướng về trang home
+        response.sendRedirect(frontendUrl + "/home"); // Thay bằng domain thực tế của frontend
     }
 
 
@@ -142,12 +183,13 @@ public class UserController {
             @RequestParam(required = false) List<String> brands,
             @RequestParam(required = false) List<String> categories,
             @RequestParam(required = false) String vehicleName,
+            @RequestParam(required = false) String fuelType,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
             @RequestParam(required = false) String status
     ) {
         Pageable pageable = PageRequest.of(page, size);
-        return BaseApiResponse.succeed(userService.rentalList(brands, categories, vehicleName, startDate, endDate, pageable, status));
+        return BaseApiResponse.succeed(userService.rentalList(brands, categories, vehicleName, startDate, endDate, pageable, status,fuelType));
     }
 
     @PostMapping
@@ -199,6 +241,7 @@ public class UserController {
         userService.deleteReview(reviewId);
         return BaseApiResponse.succeed();
     }
+
     @GetMapping("/average-rating/{vehicleId}")
     public BaseApiResponse<Double> getAverageRatingForVehicle(@PathVariable String vehicleId) {
         // Giả sử logic đã được chuyển qua ReviewService
